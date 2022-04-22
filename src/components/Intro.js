db8597b22e2504f02ad5e0371b0b617eb5cacbcb
coleenhuang <coleenhuang@gmail.com>
2022-04-22 13:46:05 -0700

    added modifications to intro text styling

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import {name, greeting} from './intro.module.css'
 
 const Intro =() => (
     <div>
-        <h1>Hello!</h1>
+      <h1 className={greeting}>Hello!</h1>
       <h2>My Name is</h2>
-      <h3>Coleen Huang</h3>
+      <h3 className={name}>Coleen Huang</h3>
       <h2>I build things for the web</h2>
     </div>
 )
